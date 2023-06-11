ec326393ed3f5c2b00c4bac6b1521ff352159878
Juan081 <jdiegotp22@gmail.com>
2023-06-11 18:17:34 -0500

    Laboratorio 11 C

---
+++ Laboratorio11C/Laboratorio11C/Views/Operacions.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Laboratorio11C.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Laboratorio11C.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operacions : ContentPage
+    {
+        public Operacions()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModelOperaciones();
+        }
+    }
+}
